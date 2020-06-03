9ab8f247afd9ebf0ad250a177d234f78a7c062f3
AJAkil <cyclosis47@gmail.com>
2020-06-03 22:32:54 +0600

    added color switching to the new boxes

--- app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -15,30 +15,43 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setListeners() {
-       val clickableViews: List<View> =
-           listOf(box_one_text, box_two_text, box_three_text, box_four_text, box_five_text,
-               constrained_layout, red_button, green_button, yellow_button)
+        val clickableViews: List<View> =
+            listOf(
+                box_one_text, box_two_text, box_three_text, box_four_text, box_five_text,
+                box_six_text, box_seven_text, box_eight_text,
+                constrained_layout, red_button, green_button, yellow_button
+            )
 
-        for (item in clickableViews){
+        for (item in clickableViews) {
             item.setOnClickListener { makeColored(it) }
         }
     }
 
     private fun makeColored(view: View) {
-        when (view.id){
-        // Boxes using Color class colors for the background
+        when (view.id) {
+            // Boxes using Color class colors for the background
             R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
 
 
-        // Boxes using the Android Color resources for background
+            // Boxes using the Android Color resources for background
             R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_orange_dark)
             R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_blue_light)
             R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_purple)
+            R.id.box_six_text -> view.setBackgroundResource(android.R.color.holo_blue_dark)
+            R.id.box_seven_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
+            R.id.box_eight_text -> view.setBackgroundResource(android.R.color.holo_red_dark)
 
-        // Boxes Using custom colors in the background
-            R.id.red_button -> box_three_text.setBackgroundResource(R.color.my_red)
-            R.id.green_button -> box_five_text.setBackgroundResource(R.color.my_green)
-            R.id.yellow_button -> box_four_text.setBackgroundResource(R.color.my_yellow)
+            // Boxes Using custom colors in the background
+            R.id.red_button -> {
+                box_three_text.setBackgroundResource(R.color.my_red)
+                box_six_text.setBackgroundResource(R.color.my_yellow)
+            }
+            R.id.green_button -> {box_five_text.setBackgroundResource(R.color.my_green)
+                box_seven_text.setBackgroundResource(R.color.my_red)}
+            R.id.yellow_button -> {
+                box_four_text.setBackgroundResource(R.color.my_yellow)
+                box_eight_text.set
+            }
             R.id.box_two_text -> view.setBackgroundResource(R.color.box_two_custom)
 
             else -> view.setBackgroundResource(R.color.bg_color)
